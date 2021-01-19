7ccb81b60800d87fccc919da891bec1354f9ee19
jaygatsby578 <marupinion7537@gmail.com>
2021-01-19 01:43:18 +0000

    6

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
     root 'hello#index'
+    devise_for :accounts
   resources :studies
   resources :board_messages
   resources :board_users
   resources :hello
-  get 'hello/login_check'
-  devise_for :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
